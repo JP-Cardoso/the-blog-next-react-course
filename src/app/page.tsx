60e763e38f24ec69392fee6418871f03b35ad947
jp-cardoso <jhonpoal@hotmail.com>
2025-12-10 22:05:46 -0300

    add: Installing lib

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import clsx from 'clsx';
+
 export default function HomePage() {
   return (
-    <></>
+    <div>
+      <h1 className={clsx('text-xl', 'font-bold', 'text-blue-600')}>
+        Texto do meu h1
+      </h1>
+    </div>
   );
 }
