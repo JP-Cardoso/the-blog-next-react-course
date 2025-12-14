b4b3a4d1e9110d13148f311465d5b5f1c09bcfad
jp-cardoso <jhonpoal@hotmail.com>
2025-12-14 14:42:19 -0300

    feat: Createing the container component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,29 @@
+import { Suspense } from 'react';
 import PostsList from '@/components/PostsList';
 import SpinLoader from '@/components/SpinLoader';
-import { Suspense } from 'react';
+import Container from '@/components/Container';
 
 export default async function HomePage() {
   return (
-    <div>
+    <Container>
       <header>
-        <h1 className='text-6xl font-bold text-center py-8'>
-          Header
-        </h1>
+        <h1 className='text-6xl font-bold text-center py-8'>Header</h1>
+        <p className='text-justify'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, enim
+          eligendi? Velit nihil sint, autem excepturi, dolorem similique
+          blanditiis asperiores esse odit quaerat distinctio ex architecto nulla
+          corrupti eos nobis! Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Commodi, enim eligendi? Velit nihil sint, autem excepturi,
+          dolorem similique blanditiis asperiores esse odit quaerat distinctio
+          ex architecto nulla corrupti eos nobis!
+        </p>
       </header>
-
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
-
       <footer>
-        <p className="text-6xl font-bold text-center py-8">
-          Footer
-        </p>
+        <p className='text-6xl font-bold text-center py-8'>Footer</p>
       </footer>
-    </div>
+    </Container>
   );
 }
---
+++ src/components/Container/index.tsx
@@ -0,0 +1,16 @@
+import clsx from 'clsx';
+import React from 'react';
+
+type ContainerProps = {
+  children: React.ReactNode;
+};
+
+export default function Container({ children }: ContainerProps) {
+  return (
+    <div className={clsx('text-slate-900', 'bg-slate-100', 'min-h-screen')}>
+      <div className='max-w-screen-lg mx-auto px-8'>
+        {children}
+      </div>
+    </div>
+  );
+}
