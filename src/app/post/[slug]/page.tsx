62433d913730b3a855c582ed6e015a2fc71f7152
jp-cardoso <jhonpoal@hotmail.com>
2025-12-29 14:42:48 -0300

    feat: Creating metadata dynamically

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,8 +1,21 @@
 import { findPostBySlugCached } from '@/lib/post/queries';
+import { Metadata } from 'next';
 
 type PostSlugPageProps = {
   params: Promise<{ slug: string }>;
 };
+
+export async function generateMetadata({
+  params,
+}: PostSlugPageProps): Promise<Metadata> {
+  const { slug } = await params;
+
+  const post = await findPostBySlugCached(slug);
+
+  return {
+    title: post.title,
+  };
+}
 
 export default async function PostSlugPage({ params }: PostSlugPageProps) {
   const { slug } = await params;
