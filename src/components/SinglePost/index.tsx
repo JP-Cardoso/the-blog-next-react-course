cc8d28eedb9d7713389f6abbb50a6f40dd412464
jp-cardoso <jhonpoal@hotmail.com>
2025-12-29 15:26:50 -0300

    style: Styling the SimplePage component

--- src/components/SinglePost/index.tsx
+++ src/components/SinglePost/index.tsx
@@ -1,4 +1,7 @@
 import { findPostBySlugCached } from '@/lib/post/queries';
+import Image from 'next/image';
+import PostDate from '../PostDate';
+import PostHeading from '../PostHeading';
 
 type SinglePostProos = {
   slug: string;
@@ -8,8 +11,26 @@ export default async function SinglePost({ slug }: SinglePostProos) {
   const post = await findPostBySlugCached(slug);
 
   return (
-    <>
-      <h1>{post?.title}</h1>
-    </>
+    <article className='mb-16'>
+      <header className='flex flex-col gap-4 mb-4'>
+        <Image
+          className='rounded-xl'
+          src={post?.coverImageUrl}
+          width={1200}
+          height={720}
+          alt={post?.title}
+        />
+
+        <PostHeading url={`/post/${post.slug}`}>{post?.title}</PostHeading>
+
+        <p>
+          {post?.author} | <PostDate createdAt={post?.createdAt} />
+        </p>
+      </header>
+
+      <p className='text-xl text-slate-600 mb-4'>{post?.excerpt}</p>
+
+      <div>{post?.content}</div>
+    </article>
   );
 }
